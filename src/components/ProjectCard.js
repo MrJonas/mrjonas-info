0e3ecbabcd4b9a2a3fc6a170e5e4863cb79eb931
mrjonas <jonas.l.antanaitis@gmail.com>
2019-10-07 20:57:40 +0300

    fixing project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, CardTitle, CardText, Col } from 'reactstrap';
+import { Card, Button, CardText, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { texts } from './../texts';
@@ -9,9 +9,9 @@ const { projects } = texts;
 export const ProjectCard = ({ name, link_url, description, github_url }) => (
     <Col sm="6">
         <Card body inverse className="mt-1" style={{ backgroundColor: '#424242', borderColor: '#424242' }}>
-            <CardTitle>
+            <h5>
                 {!!link_url ? <a href={link_url} target="_blank" rel="noopener noreferrer">{name}</a> : name}
-            </CardTitle>
+            </h5>
             <CardText>{description}</CardText>
             <a href={github_url} target="_blank" rel="noopener noreferrer">
                 <Button> <FontAwesomeIcon icon={faGithub} /> {projects.button}</Button>
